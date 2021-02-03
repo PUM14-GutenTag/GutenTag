9c63e79166901683cfa495b56df3127c2f95d76e
Adam Hjalmarsson <adahj913@student.liu.se>
2021-02-03 15:33:34 +0100

    Test

--- example.py
+++ example.py
@@ -11,6 +11,6 @@ def subtract(a, b):
     return a + b  # <--- fix this in step 8
 
 
-# uncomment the following test in step 5
-#def test_subtract():
-#    assert subtract(2, 3) == -1
+
+def test_subtract():
+    assert subtract(2, 3) == -1
