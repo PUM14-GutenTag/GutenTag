55c0fca5c79bc8e66b8e2478d94658bb1a3bfb78
Joseph Hughes <joe-joe@live.se>
2021-02-21 18:02:28 +0100

    Remove commented code

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -12,11 +12,3 @@ class HelloWorld(Resource):
 
 
 rest.add_resource(HelloWorld, '/')
-
-
-# @app.route("/", methods=["GET"])
-# def hello_world():
-#     t = Test()
-#     db.session.add(t)
-#     db.session.commit()
-#     return {"result": len(Test.query.all())}
