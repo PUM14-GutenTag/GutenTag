961a0775703128fe9f185d614947be04ee249f44
AdamHjalmarsson <AdamHjalmarsson@users.noreply.github.com>
2021-02-23 14:59:15 +0000

    autopep8 action fixes

--- test_file.py
+++ test_file.py
@@ -1,6 +1,6 @@
 
-# This test file contains all the tests that will be 
-# executed everytime someone makes a pull request or 
+# This test file contains all the tests that will be
+# executed everytime someone makes a pull request or
 # pushes to the main branch.
 
 # Test example: function(1) == 7 is correct so this
@@ -8,5 +8,7 @@
 def function(x):
     y = 6
     return x + y
+
+
 def test_function():
     assert function(1) == 7, "Test failed"
