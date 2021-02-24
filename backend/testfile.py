08b2281c0eacb8c8c8e7bb426c126f66602b9593
Adam Hjalmarsson <adahj913@student.liu.se>
2021-02-24 14:56:44 +0100

    test

--- backend/testfile.py
+++ backend/testfile.py
@@ -3,17 +3,32 @@ import sys
 
 
 def example1():
-    # This is a long comment. This should be wrapped to fit within 72 characters.
+    # This is a long comment. This should be wrapped to fit within 72
+    # characters.
     some_tuple = (1, 2, 3, 'a')
-    some_variable = {'long': 'Long code lines should be wrapped within 79 characters.',
-                     'other': [math.pi, 100, 200, 300, 9876543210, 'This is a long string that goes on'],
-                     'more': {'inner': 'This whole logical line should be wrapped.', some_tuple: [1,
-                                                                                                  20, 300, 40000, 500000000, 60000000000000000]}}
+    some_variable = {
+        'long': 'Long code lines should be wrapped within 79 characters.',
+        'other': [
+            math.pi,
+            100,
+            200,
+            300,
+            9876543210,
+            'This is a long string that goes on'],
+        'more': {
+            'inner': 'This whole logical line should be wrapped.',
+            some_tuple: [
+                1,
+                20,
+                300,
+                40000,
+                500000000,
+                60000000000000000]}}
     return (some_tuple, some_variable)
 
 
-def example2(): return {'has_key() is deprecated': True}.has_key(
-    {'f': 2}.has_key(''))
+def example2(): return (
+    '' in {'f': 2}) in {'has_key() is deprecated': True}
 
 
 class Example3(object):
