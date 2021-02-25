d46ddcd3969ff1ec59674bd3753cb3e7af33513f
Annie Wång <annwa858@student.liu.se>
2021-02-25 09:55:54 +0100

    Add documentation.

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,7 +1,11 @@
+"""
+This file contains all database models.
+"""
 import datetime
 from api import db
 
 
+# The association table between user and project.
 access_control_table = db.Table("access_control", db.Model.metadata,
     db.Column('project_id', db.Integer, db.ForeignKey('project.id')),
     db.Column('user_id', db.Integer, db.ForeignKey('users.id'))
@@ -9,6 +13,9 @@ access_control_table = db.Table("access_control", db.Model.metadata,
 
 
 class Test(db.Model):
+    """
+    This is a testing table not intended for the final product.
+    """
     __tablename__ = "test"
 
     id = db.Column(db.Integer, primary_key=True)
@@ -18,6 +25,10 @@ class Test(db.Model):
 
 
 class User(db.Model):
+    """
+    User contains information about the users, has information about which
+    projects each users are relatd to.
+    """
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
@@ -37,33 +48,48 @@ class User(db.Model):
 
 
 class Project(db.Model):
+    """
+    Project contain information about what users are related to this project
+    and what type of project it is.
+    """
     __tablename__ = "project"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), unique=True, nullable=False)
     project_type = db.Column(db.String(128), nullable=False)
-    updated = db.Column(db.DateTime)
-    created = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    updated = db.Column(db.DateTime, default=datetime.datetime.now()) # Make timestap relative to computer timezone.
+    created = db.Column(db.DateTime, default=datetime.datetime.now())
 
     users1 = db.relationship("User", secondary=access_control_table, back_populates="projects")
 
+    def __repr__(self):
+        return f"<Projectname={self.name}, Associated users={self.users1}>"
+
 
 class Project_data(db.Model):
+    """
+    Project_data contains information about what project it is related to and
+    what data it is.
+    """
     __tablename__ = "project_data"
 
     id = db.Column(db.Integer, primary_key=True)
     project_id = db.Column(db.Integer, db.ForeignKey('project.id'))
+    data = db.Column(db.String(128)) # Unsure about the string length
     prelabel = db.Column(db.String(128))
     project_type = db.Column(db.String(128), nullable=False)
-    updated = db.Column(db.DateTime)
-    created = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    updated = db.Column(db.DateTime, default=datetime.datetime.now())
+    created = db.Column(db.DateTime, default=datetime.datetime.now())
 
 
 class Label(db.Model):
+    """
+    Label contains the label for a certain piece of data.
+    """
     __tablename__ = "labels"
 
     id = db.Column(db.Integer, primary_key=True)
-    project_id = db.Column(db.Integer, db.ForeignKey('project.id')) # Is this neccessary, cause you can go through the data.
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     data_id = db.Column(db.Integer, db.ForeignKey('project_data.id'))
     label = db.Column(db.String(128), nullable=False)
--- backend/api/routes.py
+++ backend/api/routes.py
@@ -1,22 +1,42 @@
+"""
+This file contains the routes to the database.
+"""
+
+
 from api import app, rest, db
-from api.models import Test, User
+from api.models import Test, User, Project
 from flask_restful import Resource
 
 
 class TestModels(Resource):
+    """
+    This is a test class used to test the functionality of the database models.
+    """
     def get(self):
         t = Test()
         db.session.add(t)
-        db.session.commit()
 
-        user = User("Oscars", "Lonnqvist", "oscar@mail.com" + str(len(Test.query.all())))
+        user = User("Oscars",
+                    "Lonnqvist",
+                    "oscar@mail.com" + str(len(Test.query.all()))
+                    )
         db.session.add(user)
-        db.session.commit()
 
-        return {"result": repr(User.query.all())}
+        project = Project(name=str(len(Test.query.all())),
+                          project_type="Phrase")
+        db.session.add(project)
+
+        user.projects.append(project)
+
+        db.session.commit()
+        return {"result": repr(Project.query.all())}
 
 
 class Reset(Resource):
+    """
+    Reset defines an endpoint used to reset the database for use during
+    development.
+    """
     def get(self):
         db.drop_all()
         db.create_all()
