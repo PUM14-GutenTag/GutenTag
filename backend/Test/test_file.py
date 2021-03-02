b53536385f5bc8147e220dce79676b8374eb0be2
Joe <joe-joe@live.se>
2021-03-02 16:45:36 +0100

    WIP add tests

--- backend/Test/test_file.py
+++ backend/Test/test_file.py
@@ -3,6 +3,9 @@
 executed everytime someone makes a pull request or
 pushes to the main branch.
 """
+from api.database_handler import create_user, reset_db
+from api import db
+from api.models import User
 
 
 def function(x):
@@ -24,7 +27,50 @@ Name it test_*function name*.py or pytest won't find it.
 """
 
 
-########## Add integration tests here ###########
+def test_create_user():
+    reset_db()
+    # Test correctly creating an admin user.
+    ret = create_user("Oscar", "Lonnqvist", "oscar@gmail.com", True)
+    assert User.query.get(ret["id"]) is not None
+
+    # Test correctly creating an admin user.
+    ret = create_user("first", "last", "user@gmail.com", False)
+    assert User.query.get(ret["id"]) is not None
+
+    # Test duplicate email
+    ret = create_user("Oscar", "Lonnqvist", "oscar@gmail.com", True)
+    assert User.query.get(ret["id"]) is None
+
+    # Test incorrect first name type
+    ret = create_user(["Oscar"], "Lonnqvist", "name@gmail.com", True)
+    assert User.query.get(ret["id"]) is not None
 
 
-########## Add system tests here ###########
+def test_create_project():
+    reset_db()
+    ret = create_project("Project")
+
+
+def test_add_data():
+    reset_db()
+    pass
+
+
+def test_delete_project():
+    reset_db()
+    pass
+
+
+def test_authorize_user():
+    reset_db()
+    pass
+
+
+def test_deauthorize_user():
+    reset_db()
+    pass
+
+
+def test_label_data():
+    reset_db()
+    pass
