441041dd90543518ada0d739db2d71db3baefa08
John Lindström <john.lindstrom96@gmail.com>
2021-03-05 10:23:58 +0100

    Added concept for database handler integration with API

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -11,8 +11,10 @@ class register(Resource):
         self.reqparse.add_argument('password', type=str, required=True)
 
     def post(self):
-        #args = self.reqparse.parse_args()
-        # return jsonify(args)
+        args = self.reqparse.parse_args()
+        print(args)
+        # return jsonify(db_handler.create_user(args.firstname, args.lastname,
+        #  args.email))
         return jsonify({"message": "Register not implemented in API"})
 
 
@@ -24,6 +26,7 @@ class login(Resource):
 
     def get(self):
         #args = self.reqparse.parse_args()
+        # return jsonify(db_handler.authorize_user(args.username, args.password))
         return jsonify({"message": "Login not implemented in API"})
 
 
@@ -34,6 +37,8 @@ class authorize(Resource):
         self.reqparse.add_argument('project_id', type=int, required=True)
 
     def post(self):
+        #args = self.reqparse.parse_args()
+        # return jsonify(db_handler.authorize_user(args.project_id, args.user_id))
         return jsonify({"message": "Authorize not implemented in API"})
 
 
@@ -44,7 +49,10 @@ class deauthorize(Resource):
         self.reqparse.add_argument('project_id', type=int, required=True)
 
     def post(self):
+        #args = self.reqparse.parse_args()
+        # return jsonify(db_handler.deauthorize_user(args.project_id, user_id))
         return jsonify({"message": "Deauthorize not implemented in API"})
+        # return db_handler.authorize_user(args.project_id)
 
 
 class create_project(Resource):
@@ -55,6 +63,9 @@ class create_project(Resource):
         self.reqparse.add_argument('project_type', type=str, required=True)
 
     def post(self):
+        #args = self.reqparse.parse_args()
+        # return jsonify(db_handler.create_project(args.user_id, args.project_name,
+        # args.project_type))
         return jsonify({"message": "Create project not implemented in API"})
 
 
@@ -65,6 +76,8 @@ class delete_project(Resource):
         self.reqparse.add_argument('project_id', type=int, required=True)
 
     def delete(self):
+        #args = self.reqparse.parse_args()
+        # return jsonify(db_handler.create_project(args.user_id, args.project_id))
         return jsonify({"message": "Delete project not implemented in API"})
 
 
@@ -76,9 +89,11 @@ class get_data(Resource):
         self.reqparse.add_argument('amount', type=int, required=True)
 
     def get(self):
+        #args = self.reqparse.parse_args()
         return jsonify({"message": "Get data not implemented in API"})
 
 
+"""
 class label_text(Resource):
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
@@ -100,17 +115,35 @@ class label_image(Resource):
 
     def post(self):
         return jsonify({"message": "Label image not implemented in API"})
+"""
+
+
+class label_data(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('data_id', type=int, required=True)
+        self.reqparse.add_argument('user_id', type=int, required=True)
+        self.reqparse.add_argument('label', type=str, required=True)
+
+    def post(self):
+        #args = self.reqparse.parse_args()
+        # return jsonify(db_handler.label_data(args.data_id, args.user_id,
+        # args.label))
+        return jsonify({"message": "Label data not implemented in API"})
 
 
 class remove_label(Resource):
     # This might not be neccesary - potential overlap with label_text.
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument('user_id', type=int, required=True)
-        self.reqparse.add_argument('project_id', type=int, required=True)
-        self.reqparse.add_argument('data_id', type=str, required=True)
+        self.reqparse.add_argument('label_id', type=int, required=True)
+        #self.reqparse.add_argument('user_id', type=int, required=True)
+        #self.reqparse.add_argument('project_id', type=int, required=True)
+        #self.reqparse.add_argument('data_id', type=str, required=True)
 
     def delete(self):
+        #args = self.reqparse.parse_args()
+        # return jsonify(db_handler.remove_label(args.label_id))
         return jsonify({"message": "Remove label not implemented in API"})
 
 
@@ -121,11 +154,10 @@ class get_export_data(Resource):
         self.reqparse.add_argument('user_id', type=int, required=True)
         self.reqparse.add_argument('project_id', type=int, required=True)
         self.reqparse.add_argument(
-            'filter', type=str, required=True, action='append')
+            'filter', type=str, required=False, action='append')
 
     def get(self):
-        args = self.reqparse.parse_args()
-        print(args)
+        #args = self.reqparse.parse_args()
         return jsonify({"message": "Get export data not implemented in API"})
 
 
