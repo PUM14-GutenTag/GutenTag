d16b5b315367ec6e5c7d96a6c1425292a2ba4bc6
vorbrodt <maximilian.vorbrodt@hotmail.com>
2021-04-20 12:07:42 +0200

    add functionality to input box

--- frontend/src/components/DocumentClassification.jsx
+++ frontend/src/components/DocumentClassification.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
@@ -8,32 +8,41 @@ import '../css/DocumentClassification.css';
 
 const DocumentClassification = ({ data, dataPointId, nextData }) => {
   const [label, setLabel] = useState('');
+  const inputRef = useRef();
 
   const addLabel = async (event) => {
     event.preventDefault();
     await HTTPLauncher.sendCreateDocumentClassificationLabel(dataPointId, label);
-
     nextData();
+    inputRef.current.focus();
+    setLabel('');
   };
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+
   return (
     <div className="classification-container">
       <p className="text-container">{data}</p>
-
-      <Form onSubmit={addLabel}>
-        <Form.Group controlId="form.name">
-          <Form.Control
+    <hr/>
+      <Form onSubmit={addLabel} className="form">
+        <Form.Group controlId="form.name" className="form-group">
+          <input
             type="text"
             onChange={(event) => setLabel(event.target.value)}
             placeholder="Enter label..."
             required
             className="input-box"
+            ref={inputRef}
           />
+         <button className="btn btn-primary label-btn" type="submit">Button</button>
         </Form.Group>
-        <Button className="btn btn-primary" variant="success" type="submit">
-          Pres me
-        </Button>
-        <button className="amazing-button">Press me</button>
-      </Form>
+       
+        </Form>
+        <hr/>
+        <h4>Here there should be all the old labels</h4>
     </div>
   );
 };
