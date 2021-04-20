fcde8e80c2e615122838f6d706046199cafec5ad
Annie Wång <annwa858@student.liu.se>
2021-04-20 11:34:56 +0200

    add styling and search for manageusers

--- frontend/src/components/ManageUsers.jsx
+++ frontend/src/components/ManageUsers.jsx
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Table } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import HTTPLauncher from '../services/HTTPLauncher';
 
 const ManageUsers = ({ toggleCallback }) => {
   const [showUsers, setShowUsers] = useState(true);
   const [users, setUsers] = useState([]);
+  const [filter, setFilter] = useState('');
 
   async function fetchData() {
     const result = await HTTPLauncher.sendGetUsers();
     const dataArray = Object.values(result.data.users);
     const mapedDataArray = dataArray.map((userObject) => Object.values(userObject));
+    mapedDataArray.sort();
     setUsers(mapedDataArray);
   }
 
@@ -20,19 +22,68 @@ const ManageUsers = ({ toggleCallback }) => {
     }
   }, [showUsers]);
 
+  const toggleUsers = () => {
+    setShowUsers((previousValue) => !previousValue);
+  };
+
+  const filterFunc = (u) => {
+    return (
+      u[1].toUpperCase().indexOf(filter.toUpperCase()) > -1 ||
+      u[0].toUpperCase().indexOf(filter.toUpperCase()) > -1
+    );
+  };
+
+  const removeUser = (u) => {
+    console.log(u[0]);
+  };
+
   return (
     <div>
-      <h1>Manage users</h1>
-      <ul>
-        {users.map((result) => (
-          <li key={result}>
-            <div>
-              email={result[0]} name={result[1]}
-            </div>
-          </li>
-        ))}
-      </ul>
-      <Button onClick={toggleCallback}>Back</Button>
+      {showUsers ? (
+        <div>
+          <h1>Manage users</h1>
+          <Button className="dark" onClick={toggleUsers}>
+            Add user
+          </Button>
+          <Button className="dark" onClick={toggleCallback}>
+            Back
+          </Button>
+          <br />
+          <input
+            className="filter"
+            type="text"
+            onChange={(e) => setFilter(e.target.value)}
+            value={filter}
+            placeholder="Search for users..."
+          />
+          <Table className="users-table" striped borderless hover size="sm">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users
+                .filter((u) => filterFunc(u))
+                .map((result) => (
+                  <tr key={result}>
+                    <td>{result[1]}</td>
+                    <td>{result[0]}</td>
+                    <td>
+                      <Button onClick={() => removeUser(result)}>remove</Button>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </Table>
+        </div>
+      ) : (
+        <div>
+          <div>Adding user</div>
+          <Button onClick={toggleUsers}>Back</Button>
+        </div>
+      )}
     </div>
   );
 };
