1006828a43bdbd0bf848bb00617e0ff67d2b33e8
Linnéa Ivansson <liniv742@student.liu.se>
2021-04-26 13:47:57 +0200

    Add ENUM file.

---
+++ frontend/src/SettingPages.js
@@ -0,0 +1,10 @@
+// Enum for setting pages.
+const SettingPages = {
+  DEFAULT: 0,
+  CHANGE_PASSWORD: 1,
+  ADD_NEW_PROJECT: 2,
+  MANAGE_USERS: 3,
+};
+
+Object.freeze(SettingPages);
+export default SettingPages;
