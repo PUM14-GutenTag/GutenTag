3ef3c9f9aac6402422a98b4a3c9e69ac1bfced47
Oscar Lönnqvist <osclo921@student.liu.se>
2021-04-29 19:25:30 +0200

    Fixed project information shows when click on whole container

--- frontend/src/components/Project.jsx
+++ frontend/src/components/Project.jsx
@@ -24,6 +24,7 @@ const Project = ({ id, name, created, projectType, selectedColor, showEditButton
       <div
         className="project-container"
         onClick={toggleInfo}
+        aria-hidden="true"
         style={{ backgroundColor: selectedColor }}
       >
         <div className="title-container" aria-hidden="true">
