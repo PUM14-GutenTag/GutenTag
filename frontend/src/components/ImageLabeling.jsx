9f569745a766d66b77855fe38bf92548a16c79db
Joe <joe-joe@live.se>
2021-05-04 16:13:55 +0200

    Add basic image fetching

--- frontend/src/components/ImageLabeling.jsx
+++ frontend/src/components/ImageLabeling.jsx
@@ -1,19 +1,21 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import '../css/imageLabeling.css';
 import Cropper from 'react-cropper';
 import 'cropperjs/dist/cropper.css';
+import { saveAs } from 'file-saver';
 import HTTPLauncher from '../services/HTTPLauncher';
 
 // eslint-disable-next-line
-import img from '../tests/out/ILSVRC2012_val_00000003.JPEG';
-const bigJoe =
-  'https://scontent-arn2-1.xx.fbcdn.net/v/t1.6435-9/52835846_10212407730083577_1812354183186087936_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=WT0nZMJrx34AX_Lx0UU&tn=2xc4Z9PGvpJTnJ9L&_nc_ht=scontent-arn2-1.xx&oh=444c9260966921498402b6ecbb694902&oe=60AE1408';
+// import img from '../tests/out/ILSVRC2012_val_00000003.JPEG';
+// const bigJoe =
+//   'https://scontent-arn2-1.xx.fbcdn.net/v/t1.6435-9/52835846_10212407730083577_1812354183186087936_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=WT0nZMJrx34AX_Lx0UU&tn=2xc4Z9PGvpJTnJ9L&_nc_ht=scontent-arn2-1.xx&oh=444c9260966921498402b6ecbb694902&oe=60AE1408';
 // eslint-disable-next-line
-const imgg = 'https://i.imgur.com/lFd4waN.jpg';
-const ImageLabeling = () => {
+// const imgg = 'https://i.imgur.com/lFd4waN.jpg';
+const ImageLabeling = ({ data, dataPointId }) => {
   const [cropper, setCropper] = useState();
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
+  const [imgSource, setImageSource] = useState(null);
 
   const getCropData = () => {
     if (typeof cropper !== 'undefined') {
@@ -22,6 +24,25 @@ const ImageLabeling = () => {
       return [x, y, width, height];
     }
   };
+
+  const getImage = async (id) => {
+    console.log('getImage');
+    console.log(data);
+    const response = await HTTPLauncher.sendGetImageData(id);
+    console.log(response);
+    // const blob = new Blob([response.data], { type: 'image/jpeg' });
+    const source = URL.createObjectURL(response.data);
+    // console.log(blob);
+    console.log(source);
+    if (imgSource != null) URL.revokeObjectURL(imgSource);
+    setImageSource(source);
+    // saveAs(response.data, `file.jpeg`);
+  };
+
+  useEffect(() => {
+    console.log('UseEffect');
+    getImage(dataPointId);
+  }, [dataPointId]);
 
   const handleSubmit = (e) => {
     const cropData = getCropData();
@@ -48,7 +69,7 @@ const ImageLabeling = () => {
     <div className="image-container">
       <div>
         <Cropper
-          src={imgg}
+          src={imgSource}
           style={{ height: 500, width: '100%' }}
           // Cropper.js options
           initialAspectRatio={16 / 9}
