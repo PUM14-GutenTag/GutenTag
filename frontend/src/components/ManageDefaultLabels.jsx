eed83a789f6c03efc9b81cb857ab464147a5df77
Annie Wång <annwa858@student.liu.se>
2021-05-14 15:35:56 +0200

    show labels

--- frontend/src/components/ManageDefaultLabels.jsx
+++ frontend/src/components/ManageDefaultLabels.jsx
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
+import Table from 'react-bootstrap/Table';
+import { Trash } from 'react-bootstrap-icons';
 
 import HTTPLauncher from '../services/HTTPLauncher';
 
@@ -11,14 +12,17 @@ const ManageDefaultLabels = ({ projectID }) => {
   const [label, setLabel] = useState('');
   const [labels, setLabels] = useState([]);
 
+  const fetchDefaultLabels = async () => {
+    const result = await HTTPLauncher.sendGetDefaultLabel(projectID);
+    const list = Object.keys(result.data).map((key) => result.data[key].name);
+    setLabels(list);
+    console.log(labels);
+  };
+
   const submitHandler = async (event) => {
     event.preventDefault();
     const result = await HTTPLauncher.sendCreateDefaultLabel(label, projectID);
-  };
-
-  const fetchDefaultLabels = async () => {
-    const result = await HTTPLauncher.sendGetDefaultLabel(projectID);
-    console.log(result);
+    fetchDefaultLabels();
   };
 
   useEffect(() => {
@@ -27,7 +31,7 @@ const ManageDefaultLabels = ({ projectID }) => {
 
   return (
     <div>
-      <h1>Add new default label</h1>
+      <h1>Default labels</h1>
       <Form onSubmit={submitHandler}>
         <Form.Row>
           <Col style={{ justifyContent: 'center', paddingLeft: '0px' }}>
@@ -46,6 +50,24 @@ const ManageDefaultLabels = ({ projectID }) => {
           </Col>
         </Form.Row>
       </Form>
+      <Table striped borderless hover size="sm">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th className="right">Remove</th>
+          </tr>
+        </thead>
+        <tbody>
+          {labels.map((result) => (
+            <tr key={result}>
+              <td>{result}</td>
+              <td className="right">
+                <Trash className="remove" />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </div>
   );
 };
