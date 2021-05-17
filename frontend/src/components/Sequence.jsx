d72570529adc4b044ec743d891a3242f565608f3
vorbrodt <maximilian.vorbrodt@hotmail.com>
2021-05-17 15:17:54 +0200

    removed eslint-disable

--- frontend/src/components/Sequence.jsx
+++ frontend/src/components/Sequence.jsx
@@ -13,7 +13,6 @@ const Sequence = ({ data, dataPointId, getSetLabels, labels }) => {
   const [endIndex, setEndIndex] = useState('');
   const inputRef = useRef();
   const [selection, setSelection] = useState('');
-  const wordList = data.trim().split(' ');
 
   // Adds a sequnece label and resets values in input and selection box
   const addLabel = async (event) => {
@@ -31,82 +30,6 @@ const Sequence = ({ data, dataPointId, getSetLabels, labels }) => {
     inputRef.current.value = '';
     inputRef.current.focus();
     setSelection('');
-  };
-
-  // Find start index of start word in text data and end index of end word in text data
-  const findStartEndIndex = (startWord, endWord) => {
-    // index characters
-    let tempStartIndex = 0;
-    let tempEndIndex = 0;
-    // index in list
-    const startWordIndex = wordList.indexOf(startWord);
-    const endWordIndex = wordList.indexOf(endWord);
-
-    wordList.forEach((element, index) => {
-      if (index < startWordIndex) {
-        tempStartIndex += element.length + 1;
-      } else if (index === startWordIndex) {
-        tempEndIndex = tempStartIndex + element.length - 1;
-      } else if (index <= endWordIndex) {
-        tempEndIndex += element.length + 1;
-      }
-    });
-    return { start: tempStartIndex, end: tempEndIndex };
-  };
-
-  // Check if selection contains labeled data
-  const containsLabel = (selectionIndexes) => {
-    let labeledText = false;
-    labels.forEach((label) => {
-      if (
-        (selectionIndexes.start >= label.begin && selectionIndexes.start <= label.end) ||
-        (selectionIndexes.end >= label.begin && selectionIndexes.end <= label.end) ||
-        (selectionIndexes.start <= label.end && selectionIndexes.end >= label.end) ||
-        (selectionIndexes.start <= label.begin && selectionIndexes.end >= label.begin)
-      ) {
-        labeledText = true;
-      }
-    });
-    return labeledText;
-  };
-
-  /* 
-  Function handeling selection event listener. Makes sure 
-  only text data and complete unlabeled words can be selected. 
-  */
-  const handleSelection = () => {
-    const selectedText = window.getSelection();
-    if (
-      selectedText !== undefined &&
-      selectedText.anchorNode !== null &&
-      selectedText.toString() !== '' &&
-      selectedText.toString() !== ' '
-    ) {
-      const currentlySelected = selectedText.toString();
-      const tempWordList = currentlySelected.trim().split(' ');
-
-      // check that selection includes only complete words from text data
-      if (
-        wordList.includes(tempWordList[0]) &&
-        wordList.includes(tempWordList[tempWordList.length - 1]) &&
-        selectedText.anchorNode.parentNode.id === 'text-box-container'
-      ) {
-        const selectionIndexes = findStartEndIndex(
-          tempWordList[0],
-          tempWordList[tempWordList.length - 1]
-        );
-        const result = containsLabel(selectionIndexes, selectedText);
-        if (!result) {
-          setSelection(selectedText.toString());
-          setStartIndex(selectionIndexes.start);
-          setEndIndex(selectionIndexes.end);
-        } else {
-          setSelection('');
-        }
-      } else {
-        setSelection('');
-      }
-    }
   };
 
   // Highlightes word in if it has been labeled
@@ -142,12 +65,90 @@ const Sequence = ({ data, dataPointId, getSetLabels, labels }) => {
     setEndIndex('');
   }, [dataPointId]);
 
+  // Functions for handeling selection evvent listener
   useEffect(() => {
+    const wordList = data.trim().split(' ');
+
+    // Find start index of start word in text data and end index of end word in text data
+    const findStartEndIndex = (startWord, endWord) => {
+      // index characters
+      let tempStartIndex = 0;
+      let tempEndIndex = 0;
+      // index in list
+      const startWordIndex = wordList.indexOf(startWord);
+      const endWordIndex = wordList.indexOf(endWord);
+
+      wordList.forEach((element, index) => {
+        if (index < startWordIndex) {
+          tempStartIndex += element.length + 1;
+        } else if (index === startWordIndex) {
+          tempEndIndex = tempStartIndex + element.length - 1;
+        } else if (index <= endWordIndex) {
+          tempEndIndex += element.length + 1;
+        }
+      });
+      return { start: tempStartIndex, end: tempEndIndex };
+    };
+
+    // Check if selection contains labeled data
+    const containsLabel = (selectionIndexes) => {
+      let labeledText = false;
+      labels.forEach((label) => {
+        if (
+          (selectionIndexes.start >= label.begin && selectionIndexes.start <= label.end) ||
+          (selectionIndexes.end >= label.begin && selectionIndexes.end <= label.end) ||
+          (selectionIndexes.start <= label.end && selectionIndexes.end >= label.end) ||
+          (selectionIndexes.start <= label.begin && selectionIndexes.end >= label.begin)
+        ) {
+          labeledText = true;
+        }
+      });
+      return labeledText;
+    };
+
+    /* 
+    Function handeling selection event listener. Makes sure 
+    only text data and complete unlabeled words can be selected. 
+    */
+    const handleSelection = () => {
+      const selectedText = window.getSelection();
+      if (
+        selectedText !== undefined &&
+        selectedText.anchorNode !== null &&
+        selectedText.toString() !== '' &&
+        selectedText.toString() !== ' '
+      ) {
+        const currentlySelected = selectedText.toString();
+        const tempWordList = currentlySelected.trim().split(' ');
+
+        // check that selection includes only complete words from text data
+        if (
+          wordList.includes(tempWordList[0]) &&
+          wordList.includes(tempWordList[tempWordList.length - 1]) &&
+          selectedText.anchorNode.parentNode.id === 'text-box-container'
+        ) {
+          const selectionIndexes = findStartEndIndex(
+            tempWordList[0],
+            tempWordList[tempWordList.length - 1]
+          );
+          const result = containsLabel(selectionIndexes, selectedText);
+          if (!result) {
+            setSelection(selectedText.toString());
+            setStartIndex(selectionIndexes.start);
+            setEndIndex(selectionIndexes.end);
+          } else {
+            setSelection('');
+          }
+        } else {
+          setSelection('');
+        }
+      }
+    };
+
     document.addEventListener('selectionchange', handleSelection);
     return () => {
       document.removeEventListener('selectionchange', handleSelection);
     };
-    // eslint-disable-next-line
   }, [data, labels]);
 
   return (
