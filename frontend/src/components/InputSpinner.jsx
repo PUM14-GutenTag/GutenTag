95fcef141b6e7b8cbb8bc536e7a6dc784eed1c11
Oscar Lönnqvist <osclo921@student.liu.se>
2021-05-24 14:27:17 +0200

    Prettier test

--- frontend/src/components/InputSpinner.jsx
+++ frontend/src/components/InputSpinner.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { ChevronUp, ChevronDown, ChevronLeft, ChevronRight } from 'react-bootstrap-icons';
+import { ChevronLeft, ChevronRight } from 'react-bootstrap-icons';
 
 const InputSpinner = ({ amount, setAmount }) => {
   const [counter, setCounter] = useState(0);
