0dfecea9f15d83ae39136495fb25eff69ce28456
Joe <joe-joe@live.se>
2021-05-26 11:34:47 +0200

    Remove strict mode

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -5,9 +5,4 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 // Render app
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
